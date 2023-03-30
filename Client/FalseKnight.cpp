ee1ecabf32b04729306fcb30cac0b57e820ee8d4
LeeByoungHyun <leebh1126.0614@gmail.com>
2023-03-30 15:12:45 +0900

    Set False Knight Collider

--- Client/FalseKnight.cpp
+++ Client/FalseKnight.cpp
@@ -77,8 +77,7 @@ namespace ya
 		mAnimator->CreateAnimations(L"..\\Resources\\False Knight\\False Knight_Death(Head 2)\\left", Vector2::Zero, 0.1f);
 		mAnimator->CreateAnimations(L"..\\Resources\\False Knight\\False Knight_Death(Head 2)\\right", Vector2::Zero, 0.1f);
 
-		mAnimator->Play(L"False Knight_Idleleft", true);
-		mState = eFalseKnightState::Idle;
+		mState = eFalseKnightState::StunOpen;
 		mDirection = eDirection::Left;
 
 		GameObject::Initialize();
@@ -100,6 +99,24 @@ namespace ya
 		if (mTime >= 1.0f)
 		{
 			idleFlag = false;
+			runAnticipateFlag = false;
+			runFlag = false;
+			jumpAnticipateFlag = false;
+			jumpFlag = false;
+			landFlag = false;
+			attackAnticipateFlag = false;
+			attackFlag = false;
+			attackRecoverFlag = false;
+			jumpAttackUpFlag = false;
+			jumpAttackPart1Flag = false;
+			jumpAttackPart2Flag = false;
+			jumpAttackPart3Flag = false;
+			stunRollFlag = false;
+			stunRollEndFlag = false;
+			stunOpenFlag = false;
+			stunOpenedFlag = false;
+			stunHitFlag = false;
+			deathFlag = false;
 			mTime = 0.0f;
 		}
 
@@ -241,77 +258,408 @@ namespace ya
 
 	void FalseKnight::runAnticipate()
 	{
+		if (runAnticipateFlag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mCollider->SetCenter(Vector2(-250.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
 
+				mAnimator->Play(L"False Knight_Run(Anticipate)left", false);
+				runAnticipateFlag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mCollider->SetCenter(Vector2(-25.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
+
+				mAnimator->Play(L"False Knight_Run(Anticipate)right", false);
+				runAnticipateFlag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::run()
 	{
+		if (runFlag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mCollider->SetCenter(Vector2(-250.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
 
+				mAnimator->Play(L"False Knight_Runleft", false);
+				runFlag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mCollider->SetCenter(Vector2(-25.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
+
+				mAnimator->Play(L"False Knight_Runright", false);
+				runFlag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::jumpAnticipate()
 	{
+		if (jumpAnticipateFlag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mCollider->SetCenter(Vector2(-200.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
 
+				mAnimator->Play(L"False Knight_Jump(Anticipate)left", false);
+				jumpAnticipateFlag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mCollider->SetCenter(Vector2(-75.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
+
+				mAnimator->Play(L"False Knight_Jump(Anticipate)right", false);
+				jumpAnticipateFlag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::jump()
 	{
+		if (jumpFlag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mCollider->SetCenter(Vector2(-260.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
 
+				mAnimator->Play(L"False Knight_Jumpleft", false);
+				jumpFlag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mCollider->SetCenter(Vector2(-15.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
+
+				mAnimator->Play(L"False Knight_Jumpright", false);
+				jumpFlag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::land()
 	{
+		if (landFlag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mCollider->SetCenter(Vector2(-200.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
 
+				mAnimator->Play(L"False Knight_Landleft", false);
+				landFlag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mCollider->SetCenter(Vector2(-75.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
+
+				mAnimator->Play(L"False Knight_Landright", false);
+				landFlag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::attackAnticipate()
 	{
+		if (attackAnticipateFlag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mCollider->SetCenter(Vector2(-260.0f, -320.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
 
+				mAnimator->Play(L"False Knight_Attack(Anticipate)left", false);
+				attackAnticipateFlag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mCollider->SetCenter(Vector2(-15.0f, -320.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
+
+				mAnimator->Play(L"False Knight_Attack(Anticipate)right", false);
+				attackAnticipateFlag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::attack()
 	{
+		if (attackFlag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mCollider->SetCenter(Vector2(-60.0f, -320.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
 
+				mAnimator->Play(L"False Knight_Attackleft", false);
+				attackFlag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mCollider->SetCenter(Vector2(-215.0f, -320.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
+
+				mAnimator->Play(L"False Knight_Attackright", false);
+				attackFlag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::attackRecover()
 	{
+		if (attackRecoverFlag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mCollider->SetCenter(Vector2(-150.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
 
+				mAnimator->Play(L"False Knight_Attack(Recover)left", false);
+				attackRecoverFlag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mCollider->SetCenter(Vector2(-125.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
+
+				mAnimator->Play(L"False Knight_Attack(Recover)right", false);
+				attackRecoverFlag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::jumpAttackUp()
 	{
+		if (jumpAttackUpFlag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mCollider->SetCenter(Vector2(-260.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
 
+				mAnimator->Play(L"False Knight_Jump Attack(Up)left", false);
+				jumpAttackUpFlag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mCollider->SetCenter(Vector2(-15.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
+
+				mAnimator->Play(L"False Knight_Jump Attack(Up)right", false);
+				jumpAttackUpFlag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::jumpAttackPart1()
 	{
+		if (jumpAttackPart1Flag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mCollider->SetCenter(Vector2(-210.0f, -350.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
 
+				mAnimator->Play(L"False Knight_Jump Attack(Part 1)left", false);
+				jumpAttackPart1Flag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mCollider->SetCenter(Vector2(-65.0f, -350.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
+
+				mAnimator->Play(L"False Knight_Jump Attack(Part 1)right", false);
+				jumpAttackPart1Flag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::jumpAttackPart2()
 	{
+		if (jumpAttackPart2Flag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mCollider->SetCenter(Vector2(-210.0f, -350.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
 
+				mAnimator->Play(L"False Knight_Jump Attack(Part 2)left", false);
+				jumpAttackPart2Flag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mCollider->SetCenter(Vector2(-65.0f, -350.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
+
+				mAnimator->Play(L"False Knight_Jump Attack(Part 2)right", false);
+				jumpAttackPart2Flag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::jumpAttackPart3()
 	{
+		if (jumpAttackPart3Flag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mCollider->SetCenter(Vector2(-170.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
 
+				mAnimator->Play(L"False Knight_Jump Attack(Part 3)left", false);
+				jumpAttackPart3Flag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mCollider->SetCenter(Vector2(-105.0f, -300.0f));
+				mCollider->SetSize(Vector2(275.0f, 300.0f));
+
+				mAnimator->Play(L"False Knight_Jump Attack(Part 3)right", false);
+				jumpAttackPart3Flag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::stunRoll()
 	{
+		if (stunRollFlag == false)
+		{
+			mCollider->SetActive(false);
+			mCollider->SetCenter(Vector2(0.0f, 0.0f));
+			mCollider->SetSize(Vector2(0.0f, 0.0f));
 
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mAnimator->Play(L"False Knight_Stun(Roll)left", false);
+				stunRollFlag = true;
+				break;
+
+			case eDirection::Right:	// right
+				mAnimator->Play(L"False Knight_Stun(Roll)right", false);
+				stunRollFlag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::stunRollEnd()
 	{
+		if (stunRollEndFlag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mAnimator->Play(L"False Knight_Stun(Roll End)left", false);
+				stunRollEndFlag = true;
+				break;
 
+			case eDirection::Right:	// right
+				mAnimator->Play(L"False Knight_Stun(Roll End)right", false);
+				stunRollEndFlag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::stunOpen()
 	{
+		if (stunOpenFlag == false)
+		{
+			switch (mDirection)
+			{
+			case eDirection::Left:	// left
+				mAnimator->Play(L"False Knight_Stun(Open)left", false);
+				stunOpenFlag = true;
+				break;
 
+			case eDirection::Right:	// right
+				mAnimator->Play(L"False Knight_Stun(Open)right", false);
+				stunOpenFlag = true;
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
 
 	void FalseKnight::stunOpened()
